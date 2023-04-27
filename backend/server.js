d6fa366da73e0e1df1970b904bf3738ce4934069
Justin <jiheon.ham61@gmail.com>
2023-04-27 11:22:55 -0700

    added a process.env.port for heroku

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
-app.listen(8080, () => {
+
+  const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log(`Server is running on port 8080.`);
   });
